c0d32481af43134aca9223857cbf20e7c55e4569
zeratel3000 <liuhongfei@9fbank.cn>
2016-01-11 10:22:40 +0800

    当没有对应id时会崩溃

--- sample/src/main/java/cn/jiajixin/nuwasample/MainActivity.java
+++ sample/src/main/java/cn/jiajixin/nuwasample/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //当xml中没有对应id时会崩溃
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
