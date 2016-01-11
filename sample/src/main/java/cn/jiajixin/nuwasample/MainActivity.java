58185a32ea53bd43cf8d25fdb29d6158fde11f57
zeratel3000 <liuhongfei@9fbank.cn>
2016-01-11 10:16:20 +0800

    测试

--- sample/src/main/java/cn/jiajixin/nuwasample/MainActivity.java
+++ sample/src/main/java/cn/jiajixin/nuwasample/MainActivity.java
@@ -3,6 +3,8 @@ package cn.jiajixin.nuwasample;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import cn.jiajixin.nuwasample.Hello.Hello;
@@ -13,10 +15,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RelativeLayout main_rl = (RelativeLayout) findViewById(R.id.main_rl);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         TextView textView = (TextView) findViewById(R.id.textview);
         textView.setText(new Hello().say());
+
+        TextView textView3 = new TextView(this);
+        textView3.setText("喵~");
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        params.addRule(RelativeLayout.BELOW,R.id.textview);
+        main_rl.addView(textView3,params);
     }
 
 }
